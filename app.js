c157dd0c40135e994a35468bd42b3762661f8b7c
sandrabosk <sandraboskovic@hotmail.com>
2021-04-08 10:20:08 +0200

    user registration-add code

---
+++ app.js
@@ -0,0 +1,38 @@
+// ℹ️ Gets access to environment variables/settings
+// https://www.npmjs.com/package/dotenv
+require("dotenv/config");
+
+// ℹ️ Connects to the database
+require("./db");
+
+// Handles http requests (express is node js framework)
+// https://www.npmjs.com/package/express
+const express = require("express");
+
+// Handles the handlebars
+// https://www.npmjs.com/package/hbs
+const hbs = require("hbs");
+
+const app = express();
+
+// ℹ️ This function is getting exported from the config folder. It runs most middlewares
+require("./config")(app);
+
+// default value for title local
+const projectName = "basic-auth";
+const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
+
+app.locals.title = `${capitalized(projectName)} created with Ironlauncher`;
+
+// 👇 Start handling routes here
+const index = require("./routes/index");
+app.use("/", index);
+
+// authRouter needs to be added so paste the following lines:
+const authRouter = require("./routes/auth.routes"); // <== has to be added
+app.use("/", authRouter); // <== has to be added
+
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+require("./error-handling")(app);
+
+module.exports = app;
